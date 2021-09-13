8c2106a50d3745dd73d5693095198c4cbeea1150
Joel Cooper <jodycooper16@gmail.com>
2021-09-13 11:29:09 -0700

    comment table

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,7 @@
 class MeetupsController < ApplicationController
+	def index 
+		meetups=Meetup.all
+		render json: meetups
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_174931) do
+ActiveRecord::Schema.define(version: 2021_09_13_182558) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comments"
+    t.integer "like"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "meetups", force: :cascade do |t|
     t.integer "time"
