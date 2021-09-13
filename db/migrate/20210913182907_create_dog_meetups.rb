ee16e28f4dc27e1e98d7427fd435773060d19b00
Iryna Stein <isophiestein@gmail.com>
2021-09-13 12:38:26 -0600

    adde table columns

--- db/migrate/20210913182907_create_dog_meetups.rb
+++ db/migrate/20210913182907_create_dog_meetups.rb
@@ -1,7 +1,8 @@
 class CreateDogMeetups < ActiveRecord::Migration[6.1]
   def change
     create_table :dog_meetups do |t|
-
+      t.belongs_to :dog
+      t.belongs_to :meetup
       t.timestamps
     end
   end
